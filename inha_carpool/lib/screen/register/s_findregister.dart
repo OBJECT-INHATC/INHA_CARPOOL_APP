04c0437f94d77a281ef4305a68587d350a36abe6
nyulnyul <nyul0223@naver.com>
2023-08-21 16:04:41 +0900

    refector(screen) 회원가입 인하대,인하공전 구분
    -버튼을 통해 인하대,인하공전 선택하여 구분
    -registerstore 프로바이더 제작
    -findregister 아이디/비번 찾기 페이지 제작

---
+++ inha_carpool/lib/screen/register/s_findregister.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FindRegisterPage extends StatelessWidget {
+  const FindRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body :Text("비밀번호 찾기 페이지"),
+    );
+  }
+}
