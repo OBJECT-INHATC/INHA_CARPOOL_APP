44a6fbd1bac8f66bb85fb3db3210e39e55906185
nyulnyul <nyul0223@naver.com>
2023-08-24 17:37:58 +0900

    feat(screen) 이메일 인증 보내기 구현
    -이메일 인증 보내기 구현
    - 회원가입 ui 수정
    - 성별 버튼 애니메이션 수정

---
+++ inha_carpool/lib/screen/dialog/d_auth_verification.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:inha_Carpool/fragment/setting/f_setting.dart';
+import 'package:nav/nav.dart';
+
+
+
+class auth_verificationDialog extends StatelessWidget {
+  const auth_verificationDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;//화면의 가로길이
+
+    return  AlertDialog(//경고창
+      insetPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),//경고창의 내부여백
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),//모서리 둥글게
+      content: SizedBox(//경고창의 크기
+        width: width - 20,// -20을 해주는 이유는 경고창의 내부여백이 20이기 때문
+        height: 150, //경고창의 높이
+        child: Column(//열
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,//가로로 꽉 채우기
+              children: [
+                RichText(//텍스트의 일부분만 스타일 적용
+                  text: TextSpan(//텍스트 일부분에 스타일 적용
+                    children: [
+                      WidgetSpan(//위젯을 텍스트 일부분에 적용
+                        child: Icon(Icons.check_circle, color: Colors.green[400]),
+                      ),
+                      WidgetSpan(child: SizedBox(width: 10)),
+                      TextSpan(
+                        text: "확인",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.clear),
+                ),
+              ],
+            ),
+            Divider(//구분선
+              height: 1.5,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 15),
+            Text(
+              '이메일 인증 전송!!',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '이메일에서 인증을 누르시고 로그인 하여 카풀을 이용하세요!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                height: 1.7,
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        Container(
+          padding: EdgeInsets.only(right: 10),
+          child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all(Colors.green.shade400),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+                Nav.push(LoginPage());
+              },
+              child: const Text('OK')),
+        )
+      ],
+    );;
+  }
+}
+
--- inha_carpool/lib/screen/register/s_register.dart
+++ inha_carpool/lib/screen/register/s_register.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:nav/nav.dart';
 import '../../service/sv_auth.dart';
+import '../dialog/d_auth_verification.dart';
 
 /// 0824 서은율 한승완
 /// 회원 가입 페이지
@@ -34,6 +37,7 @@ class _RegisterPageState extends State<RegisterPage> {
 
   // 성별
   String? gender;
+  var genders;
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +58,8 @@ class _RegisterPageState extends State<RegisterPage> {
                       children: [
                         Container(
                           padding: const EdgeInsets.fromLTRB(15, 20, 40, 0),
+                          width: double.infinity,
+
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
@@ -88,7 +94,7 @@ class _RegisterPageState extends State<RegisterPage> {
                               focusedBorder: const UnderlineInputBorder(
                                 borderSide: BorderSide(color: Colors.blue),
                               ),
-                              labelText: '이메일',
+                              labelText: '학번',
                               suffixText: academy,
                             ),
                             onChanged: (text) {
@@ -98,23 +104,7 @@ class _RegisterPageState extends State<RegisterPage> {
                             },
                           ),
                         ),
-                        Container(
-                          height: 40,
-                          padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                minimumSize: const Size.fromHeight(50),
-                                backgroundColor: Colors.grey[200],
-                              ),
-                              child: const Text('인증번호 전송',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold)),
-                              onPressed: () {
-                                /// TODO : 0825 서은율 추가 -> 인증 메일 전송
-                              }),
-                        ),
+
 
                         Container(
                           padding: const EdgeInsets.fromLTRB(40, 10, 40, 0),
@@ -196,9 +186,13 @@ class _RegisterPageState extends State<RegisterPage> {
                               RadioListTile(
                                 title: const Text("남성"),
                                 value: "남성",
-                                groupValue: gender,
+                                groupValue: genders,
                                 onChanged: (value) {
-                                  gender = value.toString();
+                                  setState(() {
+                                    genders = value;
+                                    gender = value.toString();
+                                  });
+
                                 },
                                 fillColor:
                                     MaterialStateProperty.all(Colors.blue),
@@ -206,9 +200,12 @@ class _RegisterPageState extends State<RegisterPage> {
                               RadioListTile(
                                 title: const Text("여성"),
                                 value: "여성",
-                                groupValue: gender,
+                                groupValue: genders,
                                 onChanged: (value) {
-                                  gender = value.toString();
+                                  setState(() {
+                                    genders = value;
+                                    gender = value.toString();
+                                  });
                                 },
                                 fillColor:
                                     MaterialStateProperty.all(Colors.red),
@@ -217,42 +214,46 @@ class _RegisterPageState extends State<RegisterPage> {
                           ),
                         ),
                         // SizedBox(height: mediaHeight(context, 0.1)),
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(40, 10, 40, 20),
-                          child: Stack(
-                            children: [
-                              TextField(
-                                obscureText: true,
-                                decoration: const InputDecoration(
-                                  enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.black),
-                                  ),
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.blue),
-                                  ),
-                                  labelText: '인증 번호',
-                                ),
-                                onChanged: (text) {
-                                  // 텍스트 필드 값 변경 시 실행할 코드 작성
-                                },
-                              ),
-                              Positioned(
-                                right: 2,
-                                child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.grey[200],
-                                  ),
-                                  onPressed: () {},
-                                  child: const Text('확인',
-                                      style: TextStyle(
-                                          fontSize: 12,
-                                          color: Colors.grey,
-                                          fontWeight: FontWeight.bold)),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        // Container(
+                        //   padding: const EdgeInsets.fromLTRB(40, 10, 40, 20),
+                        //   child: Stack(
+                        //     children: [
+                        //       TextField(
+                        //         obscureText: true,
+                        //         decoration: const InputDecoration(
+                        //           enabledBorder: UnderlineInputBorder(
+                        //             borderSide: BorderSide(color: Colors.black),
+                        //           ),
+                        //           focusedBorder: UnderlineInputBorder(
+                        //             borderSide: BorderSide(color: Colors.blue),
+                        //           ),
+                        //           labelText: '인증 번호',
+                        //         ),
+                        //         onChanged: (text)async {
+                        //           // 텍스트 필드 값 변경 시 실행할 코드 작성
+                        //
+                        //
+                        //           print("인증 메일 전송");
+                        //         },
+                        //       ),
+                        //       Positioned(
+                        //         right: 2,
+                        //         child: ElevatedButton(
+                        //           style: ElevatedButton.styleFrom(
+                        //             backgroundColor: Colors.grey[200],
+                        //           ),
+                        //           onPressed: () {},
+                        //           child: const Text('확인',
+                        //               style: TextStyle(
+                        //                   fontSize: 12,
+                        //                   color: Colors.grey,
+                        //                   fontWeight: FontWeight.bold)),
+                        //         ),
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
+
                         Container(
                           height: 80,
                           padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
@@ -266,15 +267,14 @@ class _RegisterPageState extends State<RegisterPage> {
                                       fontSize: 20,
                                       color: Colors.white,
                                       fontWeight: FontWeight.bold)),
-                              onPressed: () {
+                              onPressed: () async{
 
                                 AuthService()
                                     .registerUserWithEmailandPassword(username,
                                         email, password, "dummy", gender!)
                                     .then((value) async{
                                   if (value == true) {
-                                    showSnackbar(context, Colors.green, "회원가입이 완료되었습니다. \n 메일에서 인증을 완료해주세요.");
-                                    Navigator.pop(context);
+                                    showSnackbar(context, Colors.green, "회원가입이 완료되었습니다. \n 메일 인증을 완료해주세요.");
                                   }
                                   else {
                                     showSnackbar(context, Colors.red, value);
@@ -285,6 +285,27 @@ class _RegisterPageState extends State<RegisterPage> {
 
                               }),
                         ),
+                        Container(
+                          height: 80,
+                          padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: const Size.fromHeight(50),
+                                backgroundColor: Colors.grey[200],
+                              ),
+                              child: const Text('인증번호 전송',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.bold)),
+                              onPressed: () async{
+                                await FirebaseAuth.instance.currentUser!.sendEmailVerification();
+                                Nav.push(const auth_verificationDialog());
+
+                                /// TODO : 0825 서은율 추가 -> 인증 메일 전송
+                              }),
+                        ),
+
                       ],
                     ),
                   ),
@@ -311,5 +332,6 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
     );
   }
+
 
 }
