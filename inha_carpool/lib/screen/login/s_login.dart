10d77297f5af8fad654ccc9405bb7aa835e615ae
Sanghoon Lee <nasi3611@naver.com>
2023-08-26 01:25:16 +0900

    fix(login) : 테스트 로그인 제거

--- inha_carpool/lib/screen/login/s_login.dart
+++ inha_carpool/lib/screen/login/s_login.dart
@@ -23,14 +23,10 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void initState() {
-    testFireStore();
     super.initState();
   }
 
-  testFireStore() async {
-    var test = await FirebaseFirestore.instance.collection('product').doc('nesxx8JpR39tcNEek6zp').get();
-    print(test['aa']);
-  }
+  
 
   final formKey = GlobalKey<FormState>();
 
