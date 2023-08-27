ecfc0d0d167c8225efa17bcc144a27182045a3c7
SuuuHyeon <tngutnqls6909@gmail.com>
2023-08-28 01:39:11 +0900

    feat(carpool): 내가 참여한 카풀리스트 조회 기능
    - uid를 이용해 내가 참여한 카풀리스트를 조회하는 기능 추가

--- inha_carpool/lib/screen/main/tab/carpool/f_carpool_list.dart
+++ inha_carpool/lib/screen/main/tab/carpool/f_carpool_list.dart
@@ -1,21 +1,94 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:inha_Carpool/common/Colors/app_colors.dart';
 import 'package:inha_Carpool/common/common.dart';
 import 'package:inha_Carpool/common/extension/context_extension.dart';
+import 'package:inha_Carpool/common/util/carpool.dart';
 import 'package:inha_Carpool/screen/main/tab/carpool/s_chatroom.dart';
 
 import '../../../../common/constants.dart';
 
-class CarpoolList extends StatelessWidget {
+class CarpoolList extends StatefulWidget {
   const CarpoolList({super.key});
+
+  @override
+  State<CarpoolList> createState() => _CarpoolListState();
+}
+
+class _CarpoolListState extends State<CarpoolList> {
+
+  final storage = FlutterSecureStorage();
+  late String nickName = ""; // 기본값으로 초기화
+  late String uid = "";
+  late String gender = "";
+
+  List<DocumentSnapshot> myCarpools = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+    loadCarpools();
+
+  }
+  //유저 데이터 조회
+  Future<void> _loadUserData() async {
+    nickName = await storage.read(key: "nickName") ?? "";
+    uid = await storage.read(key: "uid") ?? "";
+    gender = await storage.read(key: "gender") ?? "";
+
+    setState(() {
+      // nickName, email, gender를 업데이트했으므로 화면을 갱신합니다.
+    });
+  }
+
+  //내가 참여한 카풀 목록 조회
+  Future<void> loadCarpools() async {
+    String myID = uid; // 내 아이디
+
+    List<DocumentSnapshot> carpools =
+        await FirebaseCarpool.getCarpoolsWithMember(myID);
+
+    setState(() {
+      myCarpools = carpools;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.center,
       child: ListView.builder(
-        itemCount: 3,
+        itemCount: myCarpools.length,
         itemBuilder: (c, i) {
+          DocumentSnapshot carpool = myCarpools[i];
+
+          //카풀 날짜 및 시간 변환
+          DateTime startTime =
+              DateTime.fromMillisecondsSinceEpoch(carpool['startTime']);
+          DateTime currentTime = DateTime.now();
+          Duration difference = startTime.difference(currentTime);
+
+          String formattedStartTime =
+              DateFormat('yyyy.MM.dd HH:mm').format(startTime); // 날짜 형식으로 변환
+
+          String formattedTime;
+          if (difference.inDays >= 365) {
+            formattedTime = '${difference.inDays ~/ 365}년 후';
+          } else if (difference.inDays >= 30) {
+            formattedTime =
+                '${difference.inDays ~/ 30}달 ${difference.inDays.remainder(30)}일 이후';
+          } else if (difference.inDays >= 1) {
+            formattedTime =
+                '${difference.inDays}일 ${difference.inHours.remainder(24)}시간 이후';
+          } else if (difference.inHours >= 1) {
+            formattedTime =
+                '${difference.inHours}시간 ${difference.inMinutes.remainder(60)}분 이후';
+          } else {
+            formattedTime = '${difference.inMinutes}분 후';
+          }
+
           return GestureDetector(
             onTap: () {
               Navigator.push(
@@ -50,7 +123,7 @@ class CarpoolList extends StatelessWidget {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  '07.26/16:00 주안역-인하공전'
+                                  "${formattedStartTime}  ${carpool['startDetailPoint']} - ${carpool['endDetailPoint']}"
                                       .text
                                       .size(16)
                                       .bold
@@ -58,7 +131,7 @@ class CarpoolList extends StatelessWidget {
                                   const SizedBox(
                                     height: 6,
                                   ),
-                                  '다들 잘 오시고 계시죠?'
+                                  '더워 죽겠는데 빨리 와주세요. 젭라'
                                       .text
                                       .size(12)
                                       .bold
@@ -67,7 +140,7 @@ class CarpoolList extends StatelessWidget {
                                   const SizedBox(
                                     height: 6,
                                   ),
-                                  '2023.07.26.13:01'
+                                  '마지막 채팅 온 시간'
                                       .text
                                       .size(12)
                                       .normal
@@ -81,7 +154,7 @@ class CarpoolList extends StatelessWidget {
                       ),
                     ),
                     Column(children: [
-                      '15분전'
+                      Text('${formattedTime}')
                           .text
                           .size(12)
                           .bold
