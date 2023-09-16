f1fd286776435100a90783885705b85155d8238d
Sanghoon Lee <nasi3611@naver.com>
2023-09-16 16:33:18 +0900

    feat(cache) : 캐쉬 지우기 추가

--- inha_carpool/lib/screen/main/tab/mypage/f_mypage.dart
+++ inha_carpool/lib/screen/main/tab/mypage/f_mypage.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:inha_Carpool/common/common.dart';
+import 'package:inha_Carpool/common/extension/velocityx_extension.dart';
+import 'package:inha_Carpool/screen/main/tab/mypage/w_Menu.dart';
 import 'package:inha_Carpool/screen/main/tab/mypage/w_profile.dart';
 import 'package:inha_Carpool/screen/main/tab/mypage/w_recordList.dart';
 
 import 'package:inha_Carpool/common/data/preference/prefs.dart';
+import '../../../../common/widget/w_tap.dart';
+import '../../../dialog/d_message.dart';
 import '../../../setting/w_switch_menu.dart';
 import 'd_changepassword.dart';
 import 'f_logout_confirmation.dart';
@@ -23,6 +28,7 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
 
   bool isEventAdsAllowed = true; // 스위치의 초기 상태를 설정
+  bool isEvent = true; // 스위치의 초기 상태를 설정
 
 
 
@@ -54,6 +60,17 @@ class _MyPageState extends State<MyPage> {
                     ),
                   ],
                 ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.history_toggle_off_rounded,
+                  color: Colors.black,
+                ),
+                title: Text('이용기록'),
+                onTap: () {
+                  // 이용기록 페이지로 이동
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => recordList()));
+                },
               ),
               ListTile(
                 leading: Icon(
@@ -99,6 +116,8 @@ class _MyPageState extends State<MyPage> {
                   Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecessionPage()));
                 },
               ),
+
+
 
 
               // 알림 항목
@@ -153,17 +172,7 @@ class _MyPageState extends State<MyPage> {
                   ],
                 ),
               ),
-              ListTile(
-                leading: Icon(
-                  Icons.history_toggle_off_rounded,
-                  color: Colors.black,
-                ),
-                title: Text('이용기록'),
-                onTap: () {
-                  // 이용기록 페이지로 이동
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => recordList()));
-                },
-              ),
+
 
 
               ListTile(
@@ -197,7 +206,37 @@ class _MyPageState extends State<MyPage> {
                 },
               ),
             ),
-
+              ListTile(
+                leading: Icon(
+                  Icons.cached_outlined,
+                  color: Colors.grey,
+                ),
+                title: 'clear_cache'.tr().text.make(),
+                onTap: () async {
+                  final manager = DefaultCacheManager();
+                  await manager.emptyCache();
+                  if (mounted) {
+                    MessageDialog('clear_cache_done'.tr()).show();
+                  }
+                },
+              ),
+            ],
+          ),
+          const Line(),
+          const Height(10),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                    height: 30,
+                    width: 100,
+                    padding: const EdgeInsets.only(left: 15),
+                    child: '  © INHAtc 컴퓨터시스템과 Object 2023'
+                        .text
+                        .size(15)
+                        .semiBold
+                        .makeWithDefaultFont()),
+              ),
             ],
           ),
         ],
