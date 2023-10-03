c20f4497096ff3ce718fa7912b8a37f7a350885c
KangJiYun093 <regina091373@gmail.com>
2023-10-04 02:12:54 +0900

    feat(messagetile): 버블 형태 변경 및 이름 위치 수정

--- inha_carpool/lib/common/widget/w_messagetile.dart
+++ inha_carpool/lib/common/widget/w_messagetile.dart
@@ -33,9 +33,10 @@ class MessageTile extends StatelessWidget {
     switch (messageType) {
       case MessageType.me:
         alignment = Alignment.centerRight;
-        bubbleColor = //Colors.lightBlueAccent; //Color.fromARGB(255, 130, 11, 252);
-        //Color.fromARGB(255, 253, 205, 3);
-        Color.fromARGB(255, 70, 100, 192);
+        bubbleColor =
+            //Colors.lightBlueAccent; //Color.fromARGB(255, 130, 11, 252);
+            //Color.fromARGB(255, 253, 205, 3);
+            Color.fromARGB(255, 70, 100, 192);
         verticalPadding = 7.0;
         break;
 
@@ -56,99 +57,108 @@ class MessageTile extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
       child: messageType == MessageType.service
           ? Center(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(
-                    vertical: verticalPadding, horizontal: 16),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: bubbleColor),
-                child: (messageType == MessageType.service)
-                    ? Text(message)
-                    : Column(
-                    crossAxisAlignment: (messageType == MessageType.me)
-                        ? CrossAxisAlignment.end
-                        : CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(sender.toUpperCase()),
-                      Text(message),
-                    ]),
-              ),
-            ],
-          ))
+              child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                      vertical: verticalPadding, horizontal: 16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: bubbleColor),
+                  child: (messageType == MessageType.service)
+                      ? Text(message)
+                      : Column(
+                          crossAxisAlignment: (messageType == MessageType.me)
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                              Text(sender.toUpperCase()),
+                              Text(message),
+                            ]),
+                ),
+              ],
+            ))
           : Align(
-        alignment: alignment,
-        child: Row(
-          mainAxisAlignment: messageType == MessageType.me
-              ? MainAxisAlignment.end
-              : MainAxisAlignment.start,
-          children: [
-            if (messageType == MessageType.me)
-              Container(
-                margin: EdgeInsets.only(right: 10),
-                padding: EdgeInsets.only(left: 20, top: 30),
-                child:
-                Text(formattedTime, style: TextStyle(fontSize: 12)),
-              ),
-            ConstrainedBox(
-              constraints: BoxConstraints(
-                  maxWidth:(message.length >= 13)
-                      ? MediaQuery.of(context).size.width * 0.6
-                      :double.infinity// 최대 너비를 화면 너비의 60%로 설정
-              ),
-              child: ChatBubble(
-                padding: EdgeInsets.only(
-                    left: 20, right: 20, top: 15, bottom: 5),
-                clipper: ChatBubbleClipper10(
-                    type: messageType == MessageType.me
-                        ? BubbleType.sendBubble
-                        : BubbleType.receiverBubble),
-                backGroundColor: bubbleColor,
-                child: (messageType == MessageType.service)
-                    ? Text(message)
-                    : Column(
-                    crossAxisAlignment:
-                    (messageType == MessageType.me)
-                        ? CrossAxisAlignment.end
-                        : CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
+              alignment: alignment,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (messageType != MessageType.me) //본인일 때 이름 표시 안함
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5, top: 2),
+                      child: Text(
                         sender,
                         style: TextStyle(
-                            fontSize: 11,
-                            fontWeight: FontWeight.normal,
-                          color: messageType == MessageType.me ? Colors.white : Colors.black,),
-                      ),
-                      Text(
-                        message,
-                        style: TextStyle(
-                          fontSize: 17,
-                          color: messageType == MessageType.me ? Colors.white : Colors.black,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: -1,
+                          fontSize: 11,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
                         ),
-                        maxLines: null,
-                        softWrap: true,
                       ),
-                    ]),
+                    ),
+                  Row(
+                    mainAxisAlignment: messageType == MessageType.me
+                        ? MainAxisAlignment.end
+                        : MainAxisAlignment.start,
+                    children: [
+                      if (messageType == MessageType.me)
+                        Container(
+                          margin: EdgeInsets.only(right: 10),
+                          padding: EdgeInsets.only(left: 20, top: 10),
+                          child:
+                              Text(formattedTime, style: TextStyle(fontSize: 12)),
+                        ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                            maxWidth: (message.length >= 13)
+                                ? MediaQuery.of(context).size.width * 0.6
+                                : double.infinity // 최대 너비를 화면 너비의 60%로 설정
+                            ),
+                        child: ChatBubble(
+                          padding: EdgeInsets.only(
+                              left: 20, right: 20, top: 10, bottom: 10),
+                          clipper: ChatBubbleClipper5(
+                              type: messageType == MessageType.me
+                                  ? BubbleType.sendBubble
+                                  : BubbleType.receiverBubble),
+                          backGroundColor: bubbleColor,
+                          child: Column(
+                              crossAxisAlignment: (messageType == MessageType.me)
+                                  ? CrossAxisAlignment.end
+                                  : CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  message,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: messageType == MessageType.me
+                                        ? Colors.white
+                                        : Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: -1,
+                                  ),
+                                  maxLines: null,
+                                  softWrap: true,
+                                ),
+                              ]),
+                        ),
+                      ),
+                      if (messageType != MessageType.me)
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          padding: EdgeInsets.only(right: 20, top: 30),
+                          child: Row(
+                            children: [
+                              Text(formattedTime, style: TextStyle(fontSize: 12)),
+                            ],
+                          ),
+                        )
+                    ],
+                  ),
+                ],
               ),
             ),
-            if (messageType != MessageType.me)
-              Container(
-                margin: EdgeInsets.only(left: 10),
-                padding: EdgeInsets.only(right: 20, top: 30),
-                child: Row(
-                  children: [
-                    Text(formattedTime, style: TextStyle(fontSize: 12)),
-                  ],
-                ),
-              )
-          ],
-        ),
-      ),
     );
   }
 }
