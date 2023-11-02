af11476ebedc1b9a0a8e6d78cb3fc55fcc09b38e
hoony00 <nasi3611@naver.com>
2023-11-03 02:28:46 +0900

    fix(server) : 서버 주소 탄력적으로 변경

--- inha_carpool/lib/common/constants.dart
+++ inha_carpool/lib/common/constants.dart
@@ -2,11 +2,8 @@ export 'theme/color/abs_theme_colors.dart';
 export 'theme/shadows/abs_theme_shadows.dart';
 
 const basePath = 'assets/image';
-//const baseUrl = 'http://54.180.114.114:8080'; // API 서버의 URL
-const baseUrl = 'http://3.36.66.118:8080'; // API 서버의 URL - 1102 상훈
-//const baseUrl = 'http://15.164.210.136:8080'; // API 서버의 URL sanghoon
-//const baseUrl = 'http://3.38.102.148:8080'; // API 서버의 URL sanghoon ver2
-//const baseUrl = 'http://localhost:8080'; // API 서버의 URL sanghoon ver2
+//const baseUrl = 'http://54.180.114.114:8080'; // API 서버의 URL 영재
+const baseUrl = 'http://43.202.197.124:8080'; // API 서버의 URL - 1102 상훈 탄력적ip
 //const LocalHoonUrl = 'http://192.168.0.6:8080'; // 로컬 테스트 URL
 // const LocalHoonUrl = 'http://10.200.43.124:8080'; // 406호 테스트 URL
 //const LocalHoonUrl = 'http://10.200.52.115:8080'; // 5 207호 테스트 URL
