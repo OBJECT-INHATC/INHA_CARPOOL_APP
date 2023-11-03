1212334f6351ff17d8118fded51aa29023166854
kyj91032 <sees77770@gmail.com>
2023-11-03 20:53:53 +0900

    feat(history): 이용내역 페이지 구성, 신고하기 연결

--- inha_carpool/lib/screen/main/tab/mypage/w_recordList.dart
+++ inha_carpool/lib/screen/main/tab/mypage/w_recordList.dart
@@ -4,7 +4,10 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:inha_Carpool/common/common.dart';
 import 'package:inha_Carpool/dto/HistoryRequestDTO.dart';
+import 'package:inha_Carpool/screen/dialog/d_complainAlert.dart';
+import 'package:inha_Carpool/screen/main/tab/carpool/s_chatroom.dart';
 import 'package:inha_Carpool/service/api/ApiService.dart';
+import 'package:isar/isar.dart';
 
 class RecordList extends StatefulWidget {
 
@@ -91,62 +94,85 @@ class _RecordListState extends State<RecordList> {
                   int epoch = historyList![index].startTime;
                   DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(epoch * 1000);
                   String formattedDate = "${dateTime.month}월 ${dateTime.day}일 ${dateTime.hour}시 ${dateTime.minute}분";
-                  return Card(
-                    color: Colors.white,
-                    surfaceTintColor: Colors.transparent,
-                    elevation: 2,
-                    margin: const EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.local_taxi_rounded,
-                                color: Colors.yellow,
-                                size: 30,
-                              ),
-                              SizedBox(width: screenWidth * 0.02),
-                              Text(
-                                formattedDate, // 출발 날짜 시간
-                                style: const TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: screenHeight * 0.02),
-                          Row(
-                            children: [
-                              const Text(
-                                '출발지:',
-                                style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(width: screenWidth * 0.03),
-                              Text(
-                                historyList![index].startDetailPoint, // 출발지 정보
-                                style: const TextStyle(color: Colors.black, fontSize: 15),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: screenHeight * 0.01),
-                          Row(
-                            children: [
-                              const Text(
-                                '도착지:',
-                                style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(width: screenWidth * 0.03),
-                              Text(
-                                historyList![index].endDetailPoint, // 도착지 정보
-                                style: const TextStyle(color: Colors.black, fontSize: 15),
-                              ),
-                            ],
-                          ),
-                        ],
+
+                  String member1 = historyList[index].member1;
+                  String member2 = historyList[index].member2;
+                  String member3 = historyList[index].member3;
+
+                  return GestureDetector(
+                    child: Card(
+                      color: Colors.white,
+                      surfaceTintColor: Colors.transparent,
+                      elevation: 2,
+                      margin: const EdgeInsets.all(10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.local_taxi_rounded,
+                                  color: Colors.blue,
+                                  size: 30,
+                                ),
+                                SizedBox(width: screenWidth * 0.02),
+                                Text(
+                                  formattedDate, // 출발 날짜 시간
+                                  style: const TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: screenHeight * 0.02),
+                            Row(
+                              children: [
+                                const Text(
+                                  '출발지:',
+                                  style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(width: screenWidth * 0.03),
+                                Text(
+                                  historyList[index].startDetailPoint, // 출발지 정보
+                                  style: const TextStyle(color: Colors.black, fontSize: 15),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: screenHeight * 0.01),
+                            Row(
+                              children: [
+                                const Text(
+                                  '도착지:',
+                                  style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(width: screenWidth * 0.03),
+                                Text(
+                                  historyList[index].endDetailPoint, // 도착지 정보
+                                  style: const TextStyle(color: Colors.black, fontSize: 15),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: screenHeight * 0.01),
+                            const Row(
+                              children: [
+                                Text(
+                                  '함께 한 사람',
+                                  style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: screenHeight * 0.01),
+                            if(member1 != "" && member1.split('_')[1] != nickName)
+                              buildMemberRow(member1,historyList[index].carPoolId), // 함께한 사람 정보 리스트
+                            if(member2 != "" && member2.split('_')[1] != nickName)
+                              buildMemberRow(member2, historyList[index].carPoolId),
+                            if(member3 != "" && member3.split('_')[1] != nickName)
+                              buildMemberRow(member3, historyList[index].carPoolId),
+                          ],
+                        ),
                       ),
                     ),
                   );
@@ -158,5 +184,44 @@ class _RecordListState extends State<RecordList> {
       ),
 
     );
+
   }
+
+
+  Widget buildMemberRow(String member, String carpoolId) { // 함께한 사람 정보
+
+    final screenWidth = MediaQuery.of(context).size.width; //360
+    final screenHeight = MediaQuery.of(context).size.height; //727
+
+    return Row(
+      children: [
+        const Icon(
+          Icons.account_circle,
+          size: 35,
+          color: Colors.black,
+        ),
+        SizedBox(width: screenWidth * 0.03),
+        Text(
+          member.split('_')[1], // 함께한 사람 닉네임
+          style: const TextStyle(color: Colors.black, fontSize: 16),
+        ),
+        IconButton(
+          icon: const Icon(Icons.arrow_forward),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => ComplainAlert(
+                reportedUserNickName: member.split('_')[1], // 피신고자 닉네임
+                myId: nickName, // 신고자 닉네임 (나)
+                carpoolId: carpoolId, // 신고 카풀 아이디
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+
+
 }
