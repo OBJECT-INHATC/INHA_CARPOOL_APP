0984a87cf53419771e6f259385402095e065c579
kyj91032 <sees77770@gmail.com>
2023-11-06 23:02:29 +0900

    주소 변경

--- inha_carpool/lib/common/constants.dart
+++ inha_carpool/lib/common/constants.dart
@@ -3,6 +3,6 @@ export 'theme/shadows/abs_theme_shadows.dart';
 
 const basePath = 'assets/image';
 //const baseUrl = 'http://54.180.114.114:8080'; // API 서버의 URL 영재
-const baseUrl = 'http://13.124.239.102:8080'; // API 서버의 URL - 1102 상훈 탄력적ip
+const baseUrl = 'http://114.71.137.141:8080'; // API 서버의 URL - 1102 상훈 탄력적ip
 //const baseUrl = 'http://localhost:8080'; // API 로컬
 //const LocalHoonUrl = 'http://192.168.0.6:8080'; // 로컬 테스트 URL
