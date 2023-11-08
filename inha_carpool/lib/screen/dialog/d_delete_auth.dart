481f2043dc45cf21544dd9cb76450b6ff1f47c2a
KangJiYun093 <regina091373@gmail.com>
2023-11-09 01:58:35 +0900

    feat(delete): 회원탈퇴 다이얼로그 수정

--- inha_carpool/lib/screen/dialog/d_delete_auth.dart
+++ inha_carpool/lib/screen/dialog/d_delete_auth.dart
@@ -18,91 +18,85 @@ class _DeleteAuthDialogState extends State<DeleteAuthDialog> {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width; //화면의 가로길이
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    double heightPercentage = 0.5; // 70% 화면 높이 사용
+    double widthPercentage = 0.9; // 90% 화면 너비 사용
 
     return AlertDialog(
-      surfaceTintColor: Colors.white,
-      //경고창
-      insetPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-      //경고창의 내부여백
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      //모서리 둥글게
-      content: SizedBox(
-        //경고창의 크기
-        width: width, // -20을 해주는 이유는 경고창의 내부여백이 20이기 때문
-        // height: 150, //경고창의 높이
-        child: Column(
-          //열
-          children: [
-
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-
-
-                  SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "탈퇴하시려는 이유를 알려주세요!",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "이유를 입력해주세요",
-                        border: InputBorder.none,
-                      ),
-                      maxLines: 10,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () async{
-                      String realDelete = await AuthService().deleteAccount(widget.email, widget.password);
-                      if(realDelete == "Success"){
-                        AuthService().signOut().then((value) {
-                          Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()),
-                                (Route<dynamic> route) => false,
-                          );
-                        });} // 로그아웃
-
-
-// Carpool
-
-                    },
-                    child: Text('정말 탈퇴하기'),
-                    style: ElevatedButton.styleFrom(
-                      surfaceTintColor: Colors.transparent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 90, vertical: 10),
-                      primary: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-          ],
-        ),
+      surfaceTintColor: Colors.transparent,
+      insetPadding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      contentPadding: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
       ),
+     content: SingleChildScrollView(
+       child: Column(
+         children: [
+           Padding(
+             padding: const EdgeInsets.all(16),
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               crossAxisAlignment: CrossAxisAlignment.center,
+               children: [
+                 const SizedBox(height: 20),
+                 const Align(
+                   alignment: Alignment.centerLeft,
+                   child: Text(
+                     "탈퇴 사유를 입력해주세요!",
+                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                     textAlign: TextAlign.left,
+                   ),
+                 ),
+                 const SizedBox(height: 10),
+                 Container(
+                   width: double.infinity,
+                   padding: const EdgeInsets.all(10),
+                   decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(10),
+                     border: Border.all(color: Colors.black),
+                   ),
+                   child: TextFormField(
+                     decoration: const InputDecoration(
+                       hintText: "이유를 입력해주세요",
+                       border: InputBorder.none,
+                     ),
+                     maxLines: 10,
+                   ),
+                 ),
+                 const SizedBox(height: 20),
+                 ElevatedButton(
+                   onPressed: () async{
+                     String realDelete = await AuthService().deleteAccount(widget.email, widget.password);
+                     if(realDelete == "Success"){
+                       AuthService().signOut().then((value) {
+                         Navigator.pushAndRemoveUntil(
+                           context,
+                           MaterialPageRoute(
+                               builder: (context) => LoginPage()),
+                               (Route<dynamic> route) => false,
+                         );
+                       })
+                       ;
+                     } // 로그인
+                   },
+                   style: ElevatedButton.styleFrom(
+                     surfaceTintColor: Colors.transparent,
+                     shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(10.0),
+                     ),
+                     padding: const EdgeInsets.symmetric(horizontal: 90, vertical: 10),
+                     primary: Colors.grey[300],
+                   ),
+                   child: const Text('정말 탈퇴하기'),
+                 ),
+               ],
+             ),
+           ),
+         ],
+       ),
+     ),
     );
   }
 }
