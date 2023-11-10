9fac0b7b368fc9888e6fff504d286701c4277041
hoony00 <nasi3611@naver.com>
2023-11-10 16:29:09 +0900

    CheckPoint BETA VER.1

--- inha_carpool/lib/service/api/Api_user.dart
+++ inha_carpool/lib/service/api/Api_user.dart
@@ -7,7 +7,6 @@ import '../../common/constants.dart';
 /// 0918 이상훈 - 서버 db에 유저 정보 관련 api
 class ApiUser {
   //유저 닉네임 업데이트
-  ///TOdo: 지윤이가 고치면 이거 추가하기 (닉네임 중복체크)
   /// 신고 하기 (저장)
   Future<bool> updateUserNickname(
       String myUid, String newNickName) async {
